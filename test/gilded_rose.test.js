d7ff62419780af9e883c945c38ee75167ebd4e2d
Ovy95 <jackoverton21@hotmail.com>
2020-04-28 12:35:14 +0100

    tests added to check the value of items over time

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -16,8 +16,11 @@ describe(" updateQuality method ", function() {
     expect(items[0].sellIn).toBe(2)
     expect(items[0].quality).toBe(5);
   });
-  describe
-  it("increases the product quailty by one for the products that increase in value over time (AGED BRIE)", function() {
+
+});
+
+describe("Increases product quailty over time Aged Brie and BackStagePasses ", function() {
+  it("increases in value over time (AGED BRIE)", function() {
     const gildedRose = new Shop([new Item("Aged Brie", 20, 30)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("Aged Brie")
@@ -25,13 +28,29 @@ describe(" updateQuality method ", function() {
     expect(items[0].quality).toBe(31);
   })
 
-  it("increases the product quailty by one for the products that increase in value over time(BACKSTAGEPASSES)", function() {
+  it("increases in value over time (BACKSTAGEPASSES)", function() {
     const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 20, 30)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("Backstage passes to a TAFKAL80ETC concert")
     expect(items[0].sellIn).toBe(19)
     expect(items[0].quality).toBe(31);
   })
+
+  it("increases the product quailty by 2 if sellIn >6  <11   (BACKSTAGEPASSES)", function() {
+    let gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 6, 11)]);
+    let items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("Backstage passes to a TAFKAL80ETC concert")
+    expect(items[0].sellIn).toBe(5)
+    expect(items[0].quality).toBe(13);
+  })
+  it("increases the product quailty by 3 if sellIn >  (BACKSTAGEPASSES)", function() {
+    let gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 3, 11)]);
+    let items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("Backstage passes to a TAFKAL80ETC concert")
+    expect(items[0].sellIn).toBe(2)
+    expect(items[0].quality).toBe(14);
+  })
+
   
 
 
