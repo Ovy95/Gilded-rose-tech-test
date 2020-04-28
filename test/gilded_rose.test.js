9875646461f243bccb9cdfa569fd404a8e7a8504
Ovy95 <jackoverton21@hotmail.com>
2020-04-28 12:03:22 +0100

    Checks products increase in value over time

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -9,5 +9,30 @@ describe(" updateQuality method ", function() {
     expect(items[0].quality).toBe(19);
   });
 
+  it("Checking to see if the item decreases by 1 for both quality and Sellin days of normal items", function() {
+    const gildedRose = new Shop([new Item("Conjured Mana Cake", 3, 6)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("Conjured Mana Cake")
+    expect(items[0].sellIn).toBe(2)
+    expect(items[0].quality).toBe(5);
+  });
+  describe
+  it("increases the product quailty by one for the products that increase in value over time (AGED BRIE)", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 20, 30)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("Aged Brie")
+    expect(items[0].sellIn).toBe(19)
+    expect(items[0].quality).toBe(31);
+  })
+
+  it("increases the product quailty by one for the products that increase in value over time(BACKSTAGEPASSES)", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 20, 30)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("Backstage passes to a TAFKAL80ETC concert")
+    expect(items[0].sellIn).toBe(19)
+    expect(items[0].quality).toBe(31);
+  })
+  
+
 
 });
