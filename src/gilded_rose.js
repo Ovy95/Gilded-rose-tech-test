1199fa70be3da1fbb669a8f5d3debbe6120d54a0
Ovy95 <jackoverton21@hotmail.com>
2020-04-30 14:07:41 +0100

    Increase and Decrease are now functions which can be called

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -10,25 +10,31 @@ class Shop {
   constructor(items=[]){
     this.items = items;
   }
+
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       const item = this.items[i]
       const Max_Quality = 50
+      
       let increase_Quality = function (item) {
         if (item.quality < Max_Quality){
           item.quality += 1;
         }
       }
+      let decrease_Quality = function (item) {
+        if (item.quality > 0 ){
+          item.quality -= 1;
+        }
+      }
       
       
-
       // This first part of the code is simply just to check if the name is cheese or the concert
       if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
         //then checks the quality is greater then 0
         if (item.quality > 0) {
           // checks if the name is Sulfuras if it is then take away quality 
           if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.quality -= 1;
+            decrease_Quality(item);
           }
         }
       } else {
@@ -57,7 +63,7 @@ class Shop {
             if (item.quality > 0) {
 
               if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                item.quality -= 1;
+                decrease_Quality(item);
               }
             }
           } else {
