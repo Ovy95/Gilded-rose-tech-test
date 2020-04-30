78a51f1cc3371daede9f95d4b435f936d96c2299
Ovy95 <jackoverton21@hotmail.com>
2020-04-30 15:52:20 +0100

    3 more functions added which all are equal to the text inputs(Aged Brie, backstagepasses and Sulfuras)

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -14,7 +14,7 @@ class Shop {
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       const item = this.items[i]
-      
+
       const Max_Quality = 50
       const Min_Quality = 0
 
@@ -33,12 +33,25 @@ class Shop {
         item.sellIn -= 1;
       }
 
+      let aged_Brie = function(item) {
+        return item.name === 'Aged Brie';
+      };
+
+      let concert_tickets = function(item) {
+        return item.name === 'Backstage passes to a TAFKAL80ETC concert';
+      }
+
+      let Legendary_Item = function(item) {
+        return item.name === 'Sulfuras, Hand of Ragnaros'
+      }
+
+
       // This first part of the code is simply just to check if the name is cheese or the concert
-      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if ( (!(aged_Brie(item))) && (!(concert_tickets(item))) ) {
         //then checks the quality is greater then 0
         if (item.quality > Min_Quality) {
           // checks if the name is Sulfuras if it is then take away quality 
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
+          if ( (!(Legendary_Item(item))) ) {
             decrease_Quality(item);
           }
         }
@@ -46,7 +59,7 @@ class Shop {
 
         if (item.quality < Max_Quality) {
           increase_Quality(item);
-            if (item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.sellIn < 11 && item.quality < Max_Quality ) {
+            if (concert_tickets(item) && item.sellIn < 11 && item.quality < Max_Quality ) {
               increase_Quality(item);
               if (item.sellIn < 6 && item.quality < Max_Quality) {
                 increase_Quality(item);
@@ -55,22 +68,22 @@ class Shop {
           }
         }
           // This is used to reduces the Selling days by one
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
+      if ( (!(Legendary_Item(item))) ) {
         decrease_SellIn(item);
       }
 
         // This checks that Aged Brie increases by two after Sellin date is zero
-      if (item.sellIn < Min_Quality && item.name == 'Aged Brie'){
+      if (item.sellIn < Min_Quality && aged_Brie(item)){
         if (item.quality < Max_Quality)
           {increase_Quality(item)}
           return this.items;
       }
  
-      if (item.sellIn < Min_Quality && item.name != 'Aged Brie') {
-          if (item.sellIn < Min_Quality && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (item.sellIn < Min_Quality && (!(aged_Brie(item))) ) {
+          if (item.sellIn < Min_Quality && !(concert_tickets(item)) ) {
             if (item.quality > Min_Quality) {
 
-              if (item.name != 'Sulfuras, Hand of Ragnaros') {
+              if ( (!(Legendary_Item(item))) ) {
                 decrease_Quality(item);
               }
             }
